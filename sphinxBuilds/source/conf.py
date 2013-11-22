0c342de3d34b3642796645b02629850677ebbeb3
kylemede <kylemede@astron.s.u-tokyo.ac.jp>
2013-11-22 18:25:53 -0500

    updated to new dir

--- sphinxBuilds/source/conf.py
+++ sphinxBuilds/source/conf.py
@@ -18,7 +18,7 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
-sys.path.insert(0,os.path.abspath('/run/media/kmede/SharedData/Dropbox/workspace/CHARIS'))
+sys.path.insert(0,os.path.abspath('/run/media/kmede/SharedData/Dropbox/workspace/charis-dep'))
 
 # -- General configuration -----------------------------------------------------
 
