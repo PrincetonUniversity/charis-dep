daac0cd54fe68ff322c54b55d57b761dda0c29ed
kylemede <kylemede@astron.s.u-tokyo.ac.jp>
2013-11-22 17:26:14 -0500

    Adding initially mostly hallow primitives folder to see if commiting works from Eclipse

---
+++ primitives/__init__.py
@@ -0,0 +1,1 @@
+from primitives.calibrations import testCalPrim
---
+++ primitives/calibrations.py
@@ -0,0 +1,8 @@
+
+
+def testCalPrim():
+    """
+    Just a test prim during very early development.
+    """
+    print("this is an empty test calibration primitive")
+    
