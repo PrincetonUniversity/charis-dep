84ceed48dcb1408e0d373aa11092807e7467e53f
Kyle <Kyle@dhcp074.astron.s.u-tokyo.ac.jp>
2014-11-13 13:54:56 +0900

    added the test plotting and such file.  must stay in devel branch and not be pushed up stream.

---
+++ code/TestingFuncs.py
@@ -0,0 +1,8 @@
+"""
+Test plotting and such tools to test algorithms during development.  This toolbox and its parent folder should 
+not be pushed into the "master" branch and should always remain in the "devel" branch.
+"""
+
+import primitives as prims 
+import tools 
+
