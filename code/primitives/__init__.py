32da6b73f7c73eb7e7ac9bd121c7fbc1fcb6ea84
Timothy Brandt <tbrandt@lpr142.sns.ias.edu>
2016-04-18 22:51:07 -0400

    Added imports for buildmonochrome

--- code/primitives/__init__.py
+++ code/primitives/__init__.py
@@ -1,2 +1,3 @@
-from locate_psflets import locatePSFlets
-from fit_psflets import fit_spectra
+from locate_psflets import locatePSFlets, PSFLets
+from fit_psflets import fit_spectra, fitspec_intpix
+from buildmonochrome import gethires, make_polychrome
