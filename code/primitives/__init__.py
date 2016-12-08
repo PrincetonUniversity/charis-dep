ac7c2f5e947570f3e2b21b8bf093b837742b5d25
Timothy Brandt <tbrandt@lpr142.sns.ias.edu>
2016-12-08 15:31:56 -0500

    Removed import of masking

--- code/primitives/__init__.py
+++ code/primitives/__init__.py
@@ -1,5 +1,4 @@
 from locate_psflets import locatePSFlets, PSFLets
 from fit_psflets import fit_spectra, fitspec_intpix
 from buildmonochrome import gethires, make_polychrome
-from masking import maskbadlens
 from offset_cal import calc_offset
