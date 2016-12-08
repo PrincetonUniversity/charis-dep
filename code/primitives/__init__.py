79d239cb58280e1a6127711dbc8b6a9939349929
Timothy Brandt <tbrandt@lpr142.sns.ias.edu>
2016-12-08 14:58:53 -0500

    Added import for calc_offset

--- code/primitives/__init__.py
+++ code/primitives/__init__.py
@@ -1,3 +1,5 @@
 from locate_psflets import locatePSFlets, PSFLets
 from fit_psflets import fit_spectra, fitspec_intpix
 from buildmonochrome import gethires, make_polychrome
+from masking import maskbadlens
+from offset_cal import calc_offset
