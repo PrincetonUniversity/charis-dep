7d4dc66723f7f3f8b9fbda1034c2976040814d6e
Timothy Brandt <tbrandt@lpr142.sns.ias.edu>
2017-02-14 16:41:18 -0500

    Changed name of optimal extraction routine

--- code/primitives/__init__.py
+++ code/primitives/__init__.py
@@ -1,4 +1,4 @@
 from locate_psflets import locatePSFlets, PSFLets
-from fit_psflets import fit_spectra, fitspec_intpix
+from fit_psflets import fit_spectra, optext_spectra
 from buildmonochrome import gethires, make_polychrome
 from offset_cal import calc_offset
