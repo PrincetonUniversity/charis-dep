da11dc195389cc0247203791662910a22b98d28a
Timothy Brandt <tbrandt@mba43.wireless.ias.edu>
2017-02-15 22:10:11 -0500

    Added data files needed to build calibrations to module installation

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from setuptools import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import glob
 
 
 ext_modules = [ ]
@@ -21,9 +22,29 @@ ext_modules += [Extension("charis.utr.fitramp",
 
 setup(    
     name='charis', 
-    packages = {'charis', 'charis.primitives', 'charis.utr', 'charis.image'},
+    packages = {'charis', 'charis.primitives', 'charis.utr', 'charis.image',
+                'charis.parallel'},
     package_dir = {'charis': 'code', 'charis.primitives':'code/primitives',
-                   'charis.image':'code/image', 'charis.utr':'code/utr'},
+                   'charis.image':'code/image', 'charis.utr':'code/utr',
+                   'charis.parallel':'code/parallel'},
+    data_files = [('charis/calibrations', ['code/calibrations/lowres/mask.fits',
+                                           'code/calibrations/lowres/pixelflat.fits']),
+                  ('charis/calibrations/lowres', glob.glob('code/calibrations/lowres/hires_psflets*') + 
+                                                  ['code/calibrations/lowres/lensletflat.fits',
+                                                  'code/calibrations/lowres/lowres_tottrans.dat',
+                                                  'code/calibrations/lowres/lamsol.dat']),
+                  ('charis/calibrations/highres_J', glob.glob('code/calibrations/highres_J/hires_psflets*') + 
+                                                  ['code/calibrations/highres_J/lensletflat.fits',
+                                                  'code/calibrations/highres_J/J_tottrans.dat',
+                                                  'code/calibrations/highres_J/lamsol.dat']),
+                  ('charis/calibrations/highres_H', glob.glob('code/calibrations/highres_H/hires_psflets*') + 
+                                                  ['code/calibrations/highres_H/lensletflat.fits',
+                                                  'code/calibrations/highres_H/H_tottrans.dat',
+                                                  'code/calibrations/highres_H/lamsol.dat']),
+                  ('charis/calibrations/highres_K', glob.glob('code/calibrations/highres_K/hires_psflets*') + 
+                                                  ['code/calibrations/highres_K/lensletflat.fits',
+                                                  'code/calibrations/highres_K/K_tottrans.dat',
+                                                  'code/calibrations/highres_K/lamsol.dat'])],
     install_requires = ['numpy', 'scipy', 'astropy'],
     scripts=['scripts/buildcal', 'scripts/extractcube'],
     cmdclass = {'build_ext': build_ext},
