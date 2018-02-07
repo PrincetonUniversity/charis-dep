4cc00d3f368ae3767a9eb11ba8963ed1a8d580b4
Matthias Samland <matthias.samland@gmail.com>
2018-02-07 11:43:47 +0100

    Addressed issue #2 and #3

--- setup.py
+++ setup.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python
 
-from setuptools import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
 import glob
+from distutils.extension import Extension
 
+from Cython.Distutils import build_ext
+from setuptools import setup
 
 ext_modules_openMP = []
 ext_modules_openMP += [Extension("charis.primitives.matutils",
-                                 ['code/primitives/matutils.pyx'],
+                                 ['charis/primitives/matutils.pyx'],
                                  extra_compile_args=['-fopenmp'],
                                  extra_link_args=['-fopenmp'],
                                  )]
 
 ext_modules_openMP += [Extension("charis.utr.fitramp",
-                                 ['code/utr/fitramp.pyx'],
+                                 ['charis/utr/fitramp.pyx'],
                                  extra_compile_args=['-fopenmp'],
                                  extra_link_args=['-fopenmp'],
                                  )]
 
 ext_modules_noopenMP = []
 ext_modules_noopenMP += [Extension("charis.primitives.matutils",
-                                   ['code/primitives/matutils.pyx'],
+                                   ['charis/primitives/matutils.pyx'],
                                    )]
 
 ext_modules_noopenMP += [Extension("charis.utr.fitramp",
-                                   ['code/utr/fitramp.pyx'],
+                                   ['charis/utr/fitramp.pyx'],
                                    )]
 
 
@@ -38,27 +38,27 @@ def setup_charis(ext_modules):
         author_email='timothy.d.brandt@gmail.com',
         packages={'charis', 'charis.primitives', 'charis.utr', 'charis.image',
                     'charis.parallel'},
-        package_dir={'charis': 'code', 'charis.primitives': 'code/primitives',
-                     'charis.image': 'code/image', 'charis.utr': 'code/utr',
-                     'charis.parallel': 'code/parallel'},
-        data_files=[('charis/calibrations', ['code/calibrations/lowres/mask.fits',
-                                             'code/calibrations/lowres/pixelflat.fits']),
-                    ('charis/calibrations/lowres', glob.glob('code/calibrations/lowres/hires_psflets*') +
-                     ['code/calibrations/lowres/lensletflat.fits',
-                        'code/calibrations/lowres/lowres_tottrans.dat',
-                        'code/calibrations/lowres/lamsol.dat']),
-                    ('charis/calibrations/highres_J', glob.glob('code/calibrations/highres_J/hires_psflets*') +
-                     ['code/calibrations/highres_J/lensletflat.fits',
-                        'code/calibrations/highres_J/J_tottrans.dat',
-                        'code/calibrations/highres_J/lamsol.dat']),
-                    ('charis/calibrations/highres_H', glob.glob('code/calibrations/highres_H/hires_psflets*') +
-                     ['code/calibrations/highres_H/lensletflat.fits',
-                        'code/calibrations/highres_H/H_tottrans.dat',
-                        'code/calibrations/highres_H/lamsol.dat']),
-                    ('charis/calibrations/highres_K', glob.glob('code/calibrations/highres_K/hires_psflets*') +
-                     ['code/calibrations/highres_K/lensletflat.fits',
-                        'code/calibrations/highres_K/K_tottrans.dat',
-                        'code/calibrations/highres_K/lamsol.dat'])],
+        package_dir={'charis': 'charis', 'charis.primitives': 'charis/primitives',
+                     'charis.image': 'charis/image', 'charis.utr': 'charis/utr',
+                     'charis.parallel': 'charis/parallel'},
+        data_files=[('charis/calibrations', ['charis/calibrations/lowres/mask.fits',
+                                             'charis/calibrations/lowres/pixelflat.fits']),
+                    ('charis/calibrations/lowres', glob.glob('charis/calibrations/lowres/hires_psflets*') +
+                     ['charis/calibrations/lowres/lensletflat.fits',
+                        'charis/calibrations/lowres/lowres_tottrans.dat',
+                        'charis/calibrations/lowres/lamsol.dat']),
+                    ('charis/calibrations/highres_J', glob.glob('charis/calibrations/highres_J/hires_psflets*') +
+                     ['charis/calibrations/highres_J/lensletflat.fits',
+                        'charis/calibrations/highres_J/J_tottrans.dat',
+                        'charis/calibrations/highres_J/lamsol.dat']),
+                    ('charis/calibrations/highres_H', glob.glob('charis/calibrations/highres_H/hires_psflets*') +
+                     ['charis/calibrations/highres_H/lensletflat.fits',
+                        'charis/calibrations/highres_H/H_tottrans.dat',
+                        'charis/calibrations/highres_H/lamsol.dat']),
+                    ('charis/calibrations/highres_K', glob.glob('charis/calibrations/highres_K/hires_psflets*') +
+                     ['charis/calibrations/highres_K/lensletflat.fits',
+                        'charis/calibrations/highres_K/K_tottrans.dat',
+                        'charis/calibrations/highres_K/lamsol.dat'])],
         install_requires=['numpy', 'scipy', 'astropy'],
         scripts=['scripts/buildcal', 'scripts/extractcube'],
         cmdclass={'build_ext': build_ext},
