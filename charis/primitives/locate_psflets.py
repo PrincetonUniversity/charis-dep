2adf1473a661527ffc69c7b3e2ee0e9e262c2a71
Timothy Brandt <tbrandt@ahab.physics.ucsb.edu>
2018-04-03 13:37:59 -0700

    Fixed indexing bug

--- charis/primitives/locate_psflets.py
+++ charis/primitives/locate_psflets.py
@@ -537,13 +537,13 @@ def _transform(x, y, order, coef, highordercoef=None):
 
         i = 0
         for ix in range(order2 + 1):
-            for iy in range(order1 - ix + 1):
+            for iy in range(order2 - ix + 1):
                 if ix + iy <= order1:
                     continue
                 _x += coef[i] * x**ix * y**iy
                 i += 1
         for ix in range(order2 + 1):
-            for iy in range(order1 - ix + 1):
+            for iy in range(order2 - ix + 1):
                 if ix + iy <= order1:
                     continue
                 _y += coef[i] * x**ix * y**iy
